ab349f88981174d642cbbac643a41d2ef263f8ed
Tomi <pintertamas99@gmail.com>
2021-11-22 16:43:36 +0100

    updated the api key so the application will work even though I did not pay the RapidAPI bill

--- app/src/main/java/hu/bme/aut/feasty/utils/Constants.kt
+++ app/src/main/java/hu/bme/aut/feasty/utils/Constants.kt
@@ -4,7 +4,8 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-        const val RAPID_API_KEY = "061bd4ab6dmsh6078975ea7b1babp197554jsnc249ce8ca8a7"
+        //const val RAPID_API_KEY = "061bd4ab6dmsh6078975ea7b1babp197554jsnc249ce8ca8a7"
+        const val RAPID_API_KEY = "5be9a9444amsh643bb28e09023f5p14c283jsne79f70e9e51f"
         const val NUMBER_OF_RESULTS = 25
     }
 }
